92c2d8ead542c218e67a54518fda9c51cc64f39b
HudsonBakke <70307510+HudsonBakke@users.noreply.github.com>
2020-08-31 20:27:25 -0700

    add junk comment to rules.py

--- magicschoolgame/rules.py
+++ magicschoolgame/rules.py
@@ -1,9 +1,24 @@
 import cards
 
+class Player:
+
+    int nPlayers = 0
+
+    def __init__ (self):
+        self.n = nPlayers + 1
+        nPlayers += 1
+
+        self.attack = 0
+        self.influence = 0
+        self.health = 0
+        self.name = ""
+        self.hero_name = ""
+        self.deck = []
+        self.hand = []
+        self.played = []
+
 def initialize(config):
     pass
 
 def do_transition(old_state, transition):
-    pass
-
-#sldbf
+    game_state = {}
